1aa0ea52999a36617ef74426f91296861c6ec331
Cường Phạm <>
2024-06-14 11:44:55 +0700

    feat: init

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const app = require('./src/app')
+
+
+
+const PORT =3000
+
+
+
+const server = app.listen( PORT, () => {
+    console.log(`WSV start with port ${PORT}`);
+})
+
+process.on('SIGINT', () => {
+    server.close( () => console.log(`exits server express`))
+})
