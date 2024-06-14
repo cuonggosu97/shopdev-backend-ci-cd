67d0857fb8195f79eed54d86e77e3cb3aa99520d
Cường Phạm <>
2024-06-14 14:09:50 +0700

    feat: add api user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+
+router.get("/api/users", (req, res, next) => {
+  res.status(200).json({
+    status: "success api",
+    message: "api ok",
+    metadata: [
+      {
+        name: "cuongpham",
+        age: 28,
+      },
+      {
+        name: "Ronaldo",
+        age: 40,
+      },
+      {
+        name: "Messi",
+        age: 38,
+      },
+    ],
+  });
+});
 
 module.exports = router;
